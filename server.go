43516f98b25567800d4babb6b679a195943b43cb
Dubrovin Ruslan <dubrovin.ruslan@gmail.com>
2017-04-12 11:45:54 +0300

    simple server

--- server.go
+++ server.go
@@ -1,1 +1,53 @@
 package codingchallenge
+
+import (
+	"sync"
+	"net/http"
+	"log"
+	"encoding/json"
+)
+
+type Server struct {
+	ListenAddr string
+	Counter    int
+	wg         *sync.WaitGroup
+	mu         *sync.Mutex
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		ListenAddr: listenAddr,
+		wg: &sync.WaitGroup{},
+		mu: &sync.Mutex{},
+	}
+}
+
+func (s *Server) Run() error {
+	http.HandleFunc("/counter", serverHandler(s))
+	err := http.ListenAndServe(s.ListenAddr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+		return err
+	}
+	return nil
+}
+
+func serverHandler(s *Server) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		s.mu.Lock()
+		s.Counter += 1
+		counter := map[string]int{"Counter": s.Counter}
+		s.mu.Unlock()
+		json.NewEncoder(w).Encode(counter)
+
+	}
+}
+
+//func(s *Server) Persist(filepath string) error{
+//	w := bufio.NewWriter(filepath)
+//    	n4, err := w.WriteString("buffered\n")
+//    	fmt.Printf("wrote %d bytes\n", n4)
+//    	w.Flush()
+//}
---
+++ server_test.go
@@ -0,0 +1,40 @@
+package codingchallenge
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+type Counter struct {
+	Counter int `json:"Counter"`
+}
+func TestNewServer(t *testing.T) {
+	addr := ":8080"
+	newServer := NewServer(addr)
+	require.Equal(t, addr, newServer.ListenAddr)
+}
+
+
+func TestServerRun(t *testing.T) {
+	addr := ":8080"
+	newServer := NewServer(addr)
+	go newServer.Run()
+
+
+	var cnt Counter
+
+	for i:=0; i < 100; i++ {
+		resp, err := http.Get("http://127.0.0.1:8080/counter")
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+
+
+		body, err := ioutil.ReadAll(resp.Body)
+		err = json.Unmarshal(body, &cnt)
+		require.Nil(t, err)
+		require.Equal(t, i + 1, cnt.Counter)
+	}
+
+}
